5de66d68557768755e0624402e78b90ef7995feb
Scott <scotlandc@gmail.com>
2022-02-11 16:28:51 +1100

    azure rego details added

--- src/authConfig.js
+++ src/authConfig.js
@@ -1,8 +1,8 @@
 export const msalConfig = {
     auth: {
-      clientId: "Enter_the_Application_Id_Here",
-      authority: "Enter_the_Cloud_Instance_Id_Here/Enter_the_Tenant_Info_Here", // This is a URL (e.g. https://login.microsoftonline.com/{your tenant ID})
-      redirectUri: "Enter_the_Redirect_Uri_Here",
+      clientId: "e1d1c16f-3672-49ed-b4c3-7939c1d301d0",
+      authority: "https://login.microsoftonline.com/eeb3655d-041b-4886-aff3-9d5b01324401", // This is a URL (e.g. https://login.microsoftonline.com/{your tenant ID})
+      redirectUri: "http://localhost:3000",
     },
     cache: {
       cacheLocation: "sessionStorage", // This configures where your cache will be stored
@@ -17,5 +17,5 @@ export const msalConfig = {
 
   // Add the endpoints here for Microsoft Graph API services you'd like to use.
   export const graphConfig = {
-      graphMeEndpoint: "Enter_the_Graph_Endpoint_Here/v1.0/me"
+      graphMeEndpoint: "https://graph.microsoft.com"
   };
